fd339fc2144c9ff85cb0b8c2f2975d3f63f4dd64
Ramazan Nazkey <nazkeyramazan@gmail.com>
2022-08-24 15:45:01 +0600

    add colors array, add more mock data

--- src/components/Table/TableBody.jsx
+++ src/components/Table/TableBody.jsx
@@ -3,8 +3,16 @@ import TableRow from "@mui/material/TableRow";
 import {StyledTableCell, StyledTableBody} from "./Table";
 
 
-const colors = ['#c4c4c4', '#009999', '#ffad40', '#ec6aa1', 'green']
-
+const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+    '#FF99E6', '#CCFF1A', '#FF1A66',
+    '#66994D', '#B366CC', '#4D8000',
+    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
 
 export default function MainTableBody({rowData}){
 
@@ -13,7 +21,13 @@ export default function MainTableBody({rowData}){
             {rowData.map((row, index) => (
                 <TableRow key={index}>
 
-                    <StyledTableCell style={{backgroundColor: `${colors[row.parentID]}`}} component="th" scope="row">{row.filiale}</StyledTableCell>
+                    {row.filiale.length>11 ?
+                        <StyledTableCell style={{backgroundColor: `${colors[row.parentID]}`}} component="th" scope="row">
+                            {row.filiale.substring(0,11)}.
+                        </StyledTableCell>    :
+                        <StyledTableCell style={{backgroundColor: `${colors[row.parentID]}`}} component="th" scope="row">{row.filiale}</StyledTableCell>
+
+                    }
 
                     {row.cbo.length>5 ?
                         <StyledTableCell style={{backgroundColor: `${colors[row.parentID]}`}}>{row.cbo.substring(0,5)}.</StyledTableCell>:
@@ -25,7 +39,7 @@ export default function MainTableBody({rowData}){
 
                     {row.waitAvgTime<15?
                         <StyledTableCell style={{backgroundColor: `${colors[row.parentID]}`}}>{row.waitAvgTime}</StyledTableCell>:
-                        <StyledTableCell style={{backgroundColor: 'red'}}>{row.waitAvgTime}</StyledTableCell>
+                        <StyledTableCell style={{backgroundColor: '#ff3500'}}>{row.waitAvgTime}</StyledTableCell>
                     }
                     <StyledTableCell style={{backgroundColor: `${colors[row.parentID]}`}}>{row.queueCnt}</StyledTableCell>
                 </TableRow>
